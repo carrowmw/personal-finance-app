65ad512df58c6fde2fd82cb713cc38ae4743c312
Administrator <administrator@CMDadmins-MBP-2.neotis.local>
2024-08-21 08:42:47 +0100

    updates to posting

--- application/frontend/src/routes.py
+++ application/frontend/src/routes.py
@@ -24,7 +24,8 @@ def home():
 
 @frontend.route("/posts")
 def posts():
-    posts = Post.query.all()
+    page = request.args.get("page", 1, type=int)
+    posts = Post.query.paginate(page=page, per_page=5)
     return render_template("posts.html", posts=posts)
 
 
@@ -125,7 +126,9 @@ def new_post():
         db.session.commit()
         flash("Your post has been created!", "success")
         return redirect(url_for("frontend.home"))
-    return render_template("create_post.html", title="post.html", form=form)
+    return render_template(
+        "create_post.html", title="post.html", form=form, legend="New Post"
+    )
 
 
 @frontend.route("/post/<int:post_id>")
@@ -141,6 +144,27 @@ def update_post(post_id):
     if post.author != current_user:
         abort(403)
     form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash("Your post has been updated!", "success")
+        return redirect(url_for("frontend.post", post_id=post.id))
+    elif request.method == "GET":
+        form.title.data = post.title
+        form.content.data = post.content
     return render_template(
         "create_post.html", title="Update Post", form=form, legend="Update Post"
     )
+
+
+@frontend.route("/post/<int:post_id>/delete", methods=["POST"])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash("Your post has been deleted!", "success")
+    return redirect(url_for("frontend.home"))
